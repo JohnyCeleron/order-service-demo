77feb7d80d89c3b5e6b23ee2f723d0a658518c33
JohnyCeleron <147262524+JohnyCeleron@users.noreply.github.com>
2025-08-17 15:05:34 +0500

    Makefile and .env

--- db/init/0000_bootstrap.sql
+++ db/init/0000_bootstrap.sql
@@ -3,13 +3,13 @@
 -- роль можно через DO (это в транзакции допустимо)
 DO $$
 BEGIN
-  IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'app') THEN
-    CREATE ROLE app LOGIN PASSWORD 'secret' NOSUPERUSER NOCREATEROLE NOCREATEDB;
+  IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = '${APP_DB_USER}') THEN
+    CREATE ROLE '${APP_DB_USER}' LOGIN PASSWORD '${APP_DB_PASSWORD}' NOSUPERUSER NOCREATEROLE NOCREATEDB;
   END IF;
 END$$;
 
 -- ВАЖНО: CREATE DATABASE — отдельным выражением, без DO
-CREATE DATABASE wb_level0_db OWNER app;
+CREATE DATABASE '${APP_DB_NAME}' OWNER '${APP_DB_USER}';
 
 -- настройки роли
-ALTER ROLE app SET search_path = app, public;
+ALTER ROLE '${APP_DB_USER}' SET search_path = app, public;
